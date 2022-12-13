32341333761dec546e4ae1d23f3247cc0a0f35e8
charlotterepper <charlotte.repper01@gmail.com>
2022-12-13 11:49:47 +0100

    Create controller with index route

---
+++ TravelCool/src/main/java/com/codecool/travelcool/controller/TravelCoolController.java
@@ -0,0 +1,12 @@
+package com.codecool.travelcool.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TravelCoolController {
+    @GetMapping("/")
+    public String index() {
+        return "Start Page";
+    }
+}
