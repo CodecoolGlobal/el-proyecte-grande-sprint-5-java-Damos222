a97dcabc1f71264c1450a5d734bc31353130788e
derya9 <d.k.gesierich@gmail.com>
2022-12-13 11:34:50 +0100

    created project

---
+++ TravelCool/src/main/java/com/codecool/travelcool/TravelCoolApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.travelcool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelCoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelCoolApplication.class, args);
+	}
+
+}
