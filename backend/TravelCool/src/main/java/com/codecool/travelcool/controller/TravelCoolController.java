a60f5c27e553242ff3301b004ef0d500813f97fc
charlotterepper <charlotte.repper01@gmail.com>
2022-12-14 14:39:01 +0100

    Create StartPage component

--- backend/TravelCool/src/main/java/com/codecool/travelcool/controller/TravelCoolController.java
+++ backend/TravelCool/src/main/java/com/codecool/travelcool/controller/TravelCoolController.java
@@ -1,12 +1,18 @@
-package main.java.com.codecool.travelcool.controller;
+package com.codecool.travelcool.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TravelCoolController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String index() {
-        return "Start Page";
+        return new JSONObject()
+                .put("JSON1", "Hello World!")
+                .toString();
     }
 }
---
+++ frontend/travelcoolfront/src/components/StartPage.js
@@ -0,0 +1,10 @@
+const StartPage = () => {
+    return (
+        // Navbar
+        <div>
+            <h1>Start Page</h1>
+        </div>
+    );
+}
+
+export default StartPage;
