ebaebce774f011859b9e391211e0e048f2726f6d
charlotterepper <charlotte.repper01@gmail.com>
2022-12-14 17:53:46 +0100

    Display fetched title in navbar

--- frontend/travelcoolfront/src/components/Home.js
+++ frontend/travelcoolfront/src/components/Home.js
@@ -1,21 +1,7 @@
-import {useEffect, useState} from "react";
-
 const Home = () => {
-
-    const [title, setTitle] = useState();
-
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch("http://localhost:8080/home");
-            const data = await response.json();
-            setTitle(data);
-        }
-        fetchData();
-    }, []);
-
     return (
         <div className="home">
-            <h1>{title}</h1>
+            <h1>Home</h1>
         </div>
     );
 }
