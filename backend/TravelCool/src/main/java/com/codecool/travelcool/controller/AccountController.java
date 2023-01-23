30df1a5a1bdc08eaa3897ea752f3ccb3235176f3
charlotterepper <charlotte.repper01@gmail.com>
2023-01-23 14:02:48 +0100

    Display all accommodations on home page

--- backend/TravelCool/src/main/java/com/codecool/travelcool/controller/AccountController.java
+++ backend/TravelCool/src/main/java/com/codecool/travelcool/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.codecool.travelcool.controller;
 
-import com.codecool.travelcool.model.Account;
 import com.codecool.travelcool.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
--- backend/TravelCool/src/main/java/com/codecool/travelcool/controller/TravelCoolController.java
+++ backend/TravelCool/src/main/java/com/codecool/travelcool/controller/TravelCoolController.java
@@ -11,12 +11,7 @@ import java.util.List;
 @RestController
 public class TravelCoolController {
     @GetMapping("/home")
-    public MessageDto index() {
+    public void index() {
 
-        String message = "\"Looking for inspiration to book your next holiday? \n" +
-                " Here are some suggestions.\"";
-        MessageDto messageDto = new MessageDto();
-        messageDto.setMessage(message);
-        return messageDto;
     }
 }
