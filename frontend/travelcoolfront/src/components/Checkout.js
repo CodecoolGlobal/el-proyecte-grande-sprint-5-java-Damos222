7d4c91f7767dc9e19109aad58ad6b34617d5b90e
charlotterepper <charlotte.repper01@gmail.com>
2023-01-24 13:15:17 +0100

    Add CSS to checkout component

--- frontend/travelcoolfront/src/components/Checkout.js
+++ frontend/travelcoolfront/src/components/Checkout.js
@@ -5,8 +5,13 @@ export default function Checkout({fromDate, toDate}) {
 
     return (
         <>
-            <h2>Start Date: {fromDate}</h2>
-            <h2>End Date: {toDate}</h2>
+            <div className="checkout-area">
+                <h1>Checkout Page</h1>
+                <div className="date-area">
+                    <h3>Start Date: {fromDate}</h3>
+                    <h3>End Date: {toDate}</h3>
+                </div>
+            </div>
         </>
     );
 }
