1f53536bca1e47e344db6624deb36c71fdcb5bb6
Damos222 <david.moschitz@gmail.com>
2023-02-04 16:41:29 +0100

    resolved conflict in PhotosForm

--- frontend/travelcoolfront/src/components/AddAccommodationMultiStep/PhotosForm.js
+++ frontend/travelcoolfront/src/components/AddAccommodationMultiStep/PhotosForm.js
@@ -1,25 +1,14 @@
 import React from 'react'
-<<<<<<< HEAD
 import FormWrapper from '../FormWrapper'
 import Gallery from '../Gallery'
-=======
-import AccommodationFormWrapper from './AccommodationFormWrapper'
-import "../../css/FotosForm.css"
->>>>>>> development
 
 const PhotosForm = (props) => {
 
     return (
         <>
-<<<<<<< HEAD
-            <FormWrapper title="Fotos">
+            <FormWrapper title="Photos">
                 <label>
                     Add photos:
-=======
-            <AccommodationFormWrapper title="Fotos">
-                <label>
-                    Add fotos:
->>>>>>> development
                     <input type='file' id='images' name='images' multiple
                         onChange={(e) => {
                             props.updateImages(e.target.files)
@@ -27,22 +16,8 @@ const PhotosForm = (props) => {
                         }} required={props.images.length === 0} accept="image/*"></input>
                 </label>
                 <br></br>
-<<<<<<< HEAD
             </FormWrapper>
             <Gallery images={props.images} files={props.files} />
-=======
-            </AccommodationFormWrapper>
-            <div id='gallery'>
-                {props.images && props.images.map((image, i) => {
-                    return (
-                        <figure key={i}>
-                            <img  className='singleImage' src={image} alt='' style={{ width: "100px" }}></img>
-                            <figcaption >{props.files[i].name}</figcaption>
-                        </figure>
-                    )
-                })}
-            </div>
->>>>>>> development
         </>
     )
 }
