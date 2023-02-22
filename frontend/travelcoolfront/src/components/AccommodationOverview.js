a924eaf3533328f7881b4ea5afc9d44d045282b9
Damos222 <david.moschitz@gmail.com>
2023-02-22 16:30:56 +0100

    fixed styling

--- frontend/travelcoolfront/src/components/AccommodationOverview.js
+++ frontend/travelcoolfront/src/components/AccommodationOverview.js
@@ -1,19 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import "../css/AccommodationOverview.css"
 import Button from './Button';
 
-const AccommodationOverview = ({accommodation}) => {
+const AccommodationOverview = ({ accommodation }) => {
     const source = "data:image/jpg;base64," + accommodation.image;
     return (
-        <div className="list-accommodations">
+        <div className="list-accommodations" key={accommodation.id}>
             <div className="accommodation-image">
                 <img src={source} style={{ width: "350px" }} alt={accommodation.name + "Image"} />
             </div>
             <div className="accommodation-info">
                 <p><strong>{accommodation.name}</strong></p>
-                <span>{accommodation.address.street} {accommodation.address.houseNumber}, {accommodation.address.zipCode} 
-                {accommodation.address.city}, 
-                {accommodation.address.country}</span>
+                <span>{accommodation.address.street} {accommodation.address.houseNumber}, {accommodation.address.zipCode} {accommodation.address.city}, {accommodation.address.country}</span>
                 <p>{accommodation.description}</p>
                 <p>Capacity: {accommodation.capacity} person(s)</p>
                 <p>Type: {accommodation.type}</p>
@@ -21,11 +20,10 @@ const AccommodationOverview = ({accommodation}) => {
                     Price per night: <strong>{accommodation.pricePerNight} €</strong>
                 </p>
             </div>
-            <div className="accommodation-button">
-                <a href={"http://localhost:3000/accommodations/" + accommodation.id}>
-                    <Button type={"dark"} size={"large"} content={"See details"}></Button>
-                </a>
-            </div>
+            <Link className="accommodation-button"
+                to={"/accommodations/" + accommodation.id}>
+                <Button type={"dark"} size={"large"} content={"See details"} />
+            </Link>
         </div>
     );
 }
